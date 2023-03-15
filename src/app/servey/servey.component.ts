76d48a8393c576941974300d84ad888b0ff617c0
malith <malithhashan1997@gmail.com>
2023-03-15 20:25:28 +0530

    modal work

--- src/app/servey/servey.component.ts
+++ src/app/servey/servey.component.ts
@@ -1,28 +1,39 @@
-import {Component} from '@angular/core';
 import {ServeyServiceService} from "../service/servey-service.service";
 import {FormBuilder} from "@angular/forms";
 import {QuectionServiceService} from "../service/quection-service/quection-service.service";
 import {SharedService} from "../service/shared.service";
 import {UserAuthService} from "../service/user-auth.service";
-
+import {  ElementRef, Renderer2 } from '@angular/core';
 
 import {IQuectionNew} from "../modules/quectionModle/quectionModleForServey";
 import {UserData} from "../modules/userEnterdServeyModle";
+import {IQuection} from "../modules/quectionModle/quectionModle";
+import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-servey', templateUrl: './servey.component.html', styleUrls: ['./servey.component.css']
 })
 export class ServeyComponent {
   userId: string = '';
-  quections1: any[] = []; // Your questions data
-  answers: UserData[] = [];
-  quections: IQuectionNew[] = [];
 
-  constructor(private fb: FormBuilder, private serveyService: ServeyServiceService, private quectionservice: QuectionServiceService, private sharedService: SharedService, private userAuth: UserAuthService,) {
+  // answers: UserData[] = [];
+  quections: IQuectionNew[] = [];
+  formModal:any;
+  userResult:any;
+  UserName:any;
+  modalBody:any;
+
+  constructor(private fb: FormBuilder,
+              private serveyService: ServeyServiceService,
+              private quectionservice: QuectionServiceService,
+              private sharedService: SharedService,
+              private userAuth: UserAuthService,
+              private elRef: ElementRef,
+              private renderer: Renderer2) {
   }
 
   ngOnInit() {
-    // @ts-ignore
+
     this.UserName = this.userAuth.getUsername();
     //this.UserID = this.userAuth.getUserId();
 
@@ -35,6 +46,26 @@ export class ServeyComponent {
     }, (error) => {
       console.log(error);
     })
+
+    ;
+
+  }
+  checkTheResponce(){
+    if(this.userResult="HIGH_STRESS"){
+      this.modalBody="you are in high stress.!!!!"
+    }
+    else if(this.userResult="MEDIUM_STRESS"){
+      this.modalBody="you are in medium stress.!!!"
+    }
+    else if(this.userResult="LOW_STRESS"){
+      this.modalBody="you are in low stress.!!"
+    }
+    else if(this.userResult="NORMAL"){
+      this.modalBody="you are normal .!"
+    }
+    else if(this.userResult="CALCULATION_ERROR"){
+      this.modalBody="sorry we got a CALCULATION_ERROR .!!"
+    }
 
   }
 
@@ -58,15 +89,38 @@ export class ServeyComponent {
     }
 
 
-    this.serveyService.createServey(testModle_101).subscribe()
+    this.serveyService.createServey(testModle_101).subscribe(
+      (responce:any) => {
+        console.log(responce);
+        console.log(responce.userResult)
+        this.userResult =responce.userResult;
+      }
+    )
+    const modelDiv =document.getElementById('exampleModal');
+    if(modelDiv!=null){
+      modelDiv.style.display='block'
+      console.log("modle work")
+    }
+    const modal = this.elRef.nativeElement.querySelector('#myModal');
+    this.renderer.addClass(modal, 'show');
+    this.renderer.setStyle(modal, 'display', 'block');
 
 
     this.sharedService.setMethodID(+(this.userId))
 
+
+
+  }
+  openModal() {
+    this.formModal.show();
+  }
+  closeModal(){
+    const modal = this.elRef.nativeElement.querySelector('#myModal');
+    this.renderer.addClass(modal, 'hide');
+    this.renderer.setStyle(modal, 'display', 'none');
+    this.formModal.hide();
   }
 
 
+
 }
-
-
-
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,30 +1,65 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../service/user.service";
 import {SignUpRequestModle} from "../modules/signUpRequestModle";
 import {Router} from "@angular/router";
+import {UserAuthService} from "../service/user-auth.service";
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit{
-  constructor(private userService:UserService,
-              private router:Router
-              ) {
+export class SignUpComponent implements OnInit {
+  user = new FormGroup({
+
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(12),
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,30}$')
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(12),
+    ]),
+
+    // role: new FormControl('user', Validators.required)
+
+  });
+
+
+  constructor(
+    private userAuthService: UserAuthService,
+    private userService: UserService,
+    private router:Router
+  ) {
   }
-  ngOnInit() {
+
+  ngOnInit(): void {
   }
-  signup =new FormGroup({
-  username:new FormControl('',[Validators.required]),
-  email :new FormControl('',[Validators.required]),
-  password :new FormControl('',[Validators.required])
-  })
+
+  // matchPassword(control: AbstractControl) {
+  //   const password = control.root.get('password');
+  //   const confirmPassword = control.value;
+  //   if (password && confirmPassword && password.value !== confirmPassword) {
+  //     return { mismatchedPasswords: true };
+  //   }
+  //   return null;
+  // }
 
 
-
-  addUser(): void {
+  saveUser() {
     // // Extract the values from the form group
     // const username1 = this.signup.get('username')?.value;
     // const emai1l = this.signup.get('email')?.value;
@@ -32,10 +67,10 @@ export class SignUpComponent implements OnInit{
 
 
     const user:SignUpRequestModle=<SignUpRequestModle>{
-      username:this.signup.get('username')?.value,
-      email:this.signup.get('email')?.value,
+      username:this.user.get('username')?.value,
+      email:this.user.get('email')?.value,
       roles:['user'],
-      password:this.signup.get('password')?.value
+      password:this.user.get('password')?.value
 
     }
 
@@ -43,14 +78,14 @@ export class SignUpComponent implements OnInit{
     this.userService.signUP(user).subscribe(
       (responce:any)=> {
 
-        this.signup.reset();
+        this.user.reset();
         window.location.reload();
         console.log(responce)
         console.log("sucess")
       },
       error => {
         // alert(error.message )
-        this.signup.reset();
+        this.user.reset();
         alert("user registration Success")
         console.log("message is : "+error)
         this.router.navigate(['/SignIn'])
@@ -59,7 +94,39 @@ export class SignUpComponent implements OnInit{
     )
   }
 
-}
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   constructor(private userService:UserService,
+//               private router:Router
+//               ) {
+//   }
+//   ngOnInit() {
+//   }
+//   signup =new FormGroup({
+//   username:new FormControl('',[Validators.required]),
+//   email :new FormControl('',[Validators.required]),
+//   password :new FormControl('',[Validators.required])
+//   })
+//
+//
+//
+//   addUser(): void {
+
+//
+// }
 
 
 
